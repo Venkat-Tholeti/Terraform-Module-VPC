ce2cf39504611f3f4d5659e623098db604c9d67a
venkat <devopsaws84s@gmail.com>
2026-02-07 21:28:23 +0530

    Terraform

--- Outputs.tf
+++ Outputs.tf
@@ -9,3 +9,11 @@ output "vpc_id"{
 output "public_subnet_id"{
   value = aws_subnet.public[*].id
 }
+
+output "private_subnet_id"{
+  value = aws_subnet.private[*].id
+}
+
+output "public_database_id"{
+  value = aws_subnet.database[*].id
+}
